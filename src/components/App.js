9d47f9bf6007be9b0e9d5d868356ef4b4a5a4b7c
Artem Sukhorukov <54895933+Artem-SP@users.noreply.github.com>
2021-01-13 09:06:42 +0200

    useRef

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Header from "./Header";
 import PostList from "./PostList";
 import Footer from "./Footer";
 import { unstable_renderSubtreeIntoContainer } from "react-dom";
 
 function App() {
+  const ref = useRef(1);
   const [posts, setPost] = useState([]);
   const [theme, setTeme] = useState("light");
   const [check, setCheck] = useState(false);
@@ -31,8 +32,17 @@ function App() {
     };
   }, [type]);
 
+  const handlerFocus = () => {
+    ref.current.style.color = "red";
+  };
+
   return (
     <div className={`app ${theme}`}>
+      <div className="form">
+        <input ref={ref} />
+        <button onClick={handlerFocus}>Focus</button>
+      </div>
+
       <Header changeType={setType} check={check} changeTheme={change} />
       <PostList posts={posts} />
       <Footer />
